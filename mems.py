e56676968d045c2c88a4366588b70b0ead053818
Your Name <you@example.com>
2022-12-06 22:03:44 +0200

    сделал сайт "https://bugaga.ru" релиз

--- mems.py
+++ mems.py
@@ -16,7 +16,7 @@ def main():
         "user-agent": ua.chrome
     }
     domen = "https://bugaga.ru"
-    print(headers["user-agent"])
+    # print(headers["user-agent"])
     for x in range(1, 29):
         uri = f"/tags/%D0%BC%D0%B5%D0%BC%D1%8B/page/{x}"
         # print(uri + " " + str(datetime.datetime.now()))
@@ -30,36 +30,44 @@ def main():
                 for div in divs:
                     try:
                         pack_url = div.find("a", class_="ui-button dalee").get("href")
-                        print(pack_url)
-                        title = str(div.find("div", class_="w_tit").find("a").text)
-                        try:
-                            memes_num = int(title.split("(")[1].split(" ")[0])
-                            iner_pages = memes_num // 15
-                            print(iner_pages)
-                            for p in range(1, iner_pages + 1):
-                                part1 = pack_url.split("/interesting/")[0] + "/interesting/"
-                                part2 = pack_url.split("/interesting/")[1]
-                                pack_url = pack_url if p == 1 else f"{part1}page,{p},{part2}"
-                                print(pack_url)
-                                res = requests.get(pack_url, headers=headers)
-                                if res.status_code == 200:
-                                    print(res.status_code)
-                                    logging.info(f'Рабатаю с ссылкой - {pack_url}')
-                                    soup = BeautifulSoup(res.text, "lxml")
-                                    urls = soup.find("div", class_="w_cntn").find_all("a", class_="highslide")
-                                    print(len(urls))
-                                    if len(urls) > 0:
-                                        for a in urls:
-                                            img_url = domen + a.find("img").get("src")
-                                            mem_info_dict["name"] = ""
-                                            mem_info_dict["img"] = img_url
-                                            buffer = mem_info_dict.copy()
-                                            memes_list.append(buffer)
-                                            mem_info_dict.clear()
 
-                        except Exception as ee:
-                            logging.error(f"не перевелось в число количество мемов в паке {ee}")
-                            memes_num = None
+                        res = requests.get(pack_url, headers=headers)
+                        if res.status_code == 200:
+                            print(res.status_code)
+                            print(f'Рабатаю с ссылкой - {pack_url}')
+                            logging.info(f'Рабатаю с ссылкой - {pack_url}')
+                            soup = BeautifulSoup(res.text, "lxml")
+                            urls = soup.find("div", class_="w_cntn").find_all("a", class_="highslide")
+                            paginate = soup.find("div", class_="navigation")
+                            print(paginate)
+                            print(len(urls))
+                            if len(urls) > 0:
+                                for a in urls:
+                                    img_url = domen + a.find("img").get("src")
+                                    mem_info_dict["name"] = ""
+                                    mem_info_dict["img"] = img_url
+                                    buffer = mem_info_dict.copy()
+                                    memes_list.append(buffer)
+                                    mem_info_dict.clear()
+                                if paginate:
+                                    paginate_urls = paginate.find_all("a")
+                                    for pag_url in paginate_urls:
+                                        res = requests.get(pag_url.get("href"), headers=headers)
+                                        if res.status_code == 200:
+                                            print(res.status_code)
+                                            print(f'Рабатаю с ссылкой - {pack_url}')
+                                            logging.info(f'Рабатаю с ссылкой - {pack_url}')
+                                            soup = BeautifulSoup(res.text, "lxml")
+                                            urls = soup.find("div", class_="w_cntn").find_all("a", class_="highslide")
+                                            print(len(urls))
+                                            if len(urls) > 0:
+                                                for a in urls:
+                                                    img_url = domen + a.find("img").get("src")
+                                                    mem_info_dict["name"] = ""
+                                                    mem_info_dict["img"] = img_url
+                                                    buffer = mem_info_dict.copy()
+                                                    memes_list.append(buffer)
+                                                    mem_info_dict.clear()
 
                     except Exception as e:
                         logging.error("проблемма с запросом на страницу мемов")
@@ -67,12 +75,11 @@ def main():
         except Exception as e:
             print(e)
             logging.error(f"Проблеммы с requests на странице со сборниками мемов - {e}")
-    for x in range(0, len(memes_list)):
-        print(memes_list[x]["img"])
-        print("\n")
+
     try:
         with open("bugaga_memes.json", "w", encoding="utf-8") as f:
-            json.dump(memes_list, f, indent=4)
+            json.dump(memes_list, f, indent=4, ensure_ascii=False)
+        logging.info(f'записал в файл {len(memes_list)} записей')
     except Exception as ee:
         logging.error(f"Проблеммы с записью в файл - {ee}")
 
