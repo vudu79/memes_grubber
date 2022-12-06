729ad972b9c636191f7b20085607d8448f4938e8
Andrey <vudu1979@gmail.com>
2022-12-06 22:30:12 +0200

    finish

--- venvMems/bin/activate.fish
+++ venvMems/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/andrey/python/memes_grubber/venvMems"
+set -gx VIRTUAL_ENV "/home/andrey/python/mems_grubber2/venvMems"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
